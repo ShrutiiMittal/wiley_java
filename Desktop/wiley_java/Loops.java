5c6760974e46895dd85f317da344ae748d6f504e
ShrutiiMittal <mittalshruti007@gmail.com>
2023-04-20 16:55:11 +0530

    loops added

---
+++ Desktop/wiley_java/Loops.java
@@ -0,0 +1,17 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<5;j++)
+            System.out.println("Value of i is: "+i+"value of j is :"+j);
+     
+     
+        }
+        // while loop to print 10 numbers
+        int i=0;
+        while(i<10)
+        {
+            System.out.println("value of i is "+ i++);
+        }
+    }
+}
